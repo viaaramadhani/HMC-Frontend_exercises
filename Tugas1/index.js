f435dab42286f2cb2d75cacae08d58fdb2e6c90e
Octavia. <ramadhanivia489@gmail.com>
2024-09-09 19:00:42 +0800

    Tugas 1

---
+++ Tugas1/index.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+let nama = "Octavia Ramadhani";
+let umur = "19 Tahun";
+let apakahPelajar = true;
+
+console.log ("Nama: " + nama);
+console.log ("Umur: " + umur);
+console.log ("Apakah pelajar: " + apakahPelajar);
+
+let a = 10;
+let b = 5;
+let c = a + b;
+
+console.log("Nilai a = " + a);
+console.log("Nilai b = " + b);
+console.log("Nilai c (a + b) = " + c);
